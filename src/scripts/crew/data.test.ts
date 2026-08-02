142c2642635dd96fce24273e4810ec743f7e6983
Jason Godesky <jefgodesky@gmail.com>
2026-08-02 16:37:16 -0400

    Add new interface: CrewData

---
+++ src/scripts/crew/data.test.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest'
+import { primitives } from '@revolutionarygamesco/common/testing'
+import { createOfficerAssignments } from '../officers/assignments.ts'
+import { createProvisions } from '../provisions/provisions.ts'
+import { isCrewData, createCrewData } from './data.ts'
+
+describe('isCrewData', () => {
+  it.each(primitives)('rejects %s', (_label, value) => {
+    expect(isCrewData(value)).toBe(false)
+  })
+
+  it('accepts crew data', () => {
+    expect(isCrewData({
+      articles: [],
+      officers: createOfficerAssignments(),
+      watchCrews: [
+        { name: 'Starboard Crew', lead: 'quartermaster', members: [], onDuty: true },
+        { name: 'Larboard Crew', lead: 'master', members: [], onDuty: false },
+      ],
+      provisions: createProvisions(),
+      stock: 0
+    })).toBe(true)
+  })
+})
+
+describe('createCrewData', () => {
+  it('creates crew data', () => {
+    expect(isCrewData(createCrewData())).toBe(true)
+  })
+})
---
+++ src/scripts/crew/data.ts
@@ -0,0 +1,45 @@
+import { getObjectRecord, isStringArray, isNumber } from '@revolutionarygamesco/common'
+import { scopeLocalizer } from '@revolutionarygamesco/common-foundryvtt'
+import { MODULE_ID } from '../settings.ts'
+import { isOfficerAssignments, createOfficerAssignments, type OfficerAssignments } from '../officers/assignments.ts'
+import { isWatchCrews, type WatchCrew } from './watch-crew.ts'
+import { isProvisions, createProvisions, type Provisions } from '../provisions/provisions.ts'
+
+export interface CrewData {
+  articles: string[]
+  officers: OfficerAssignments,
+  watchCrews: WatchCrew[],
+  provisions: Provisions,
+  stock: number
+}
+
+export const isCrewData = (
+  candidate: unknown
+): candidate is CrewData => {
+  const obj = getObjectRecord(candidate)
+  if (!obj) return false
+  return [
+    isStringArray(obj.articles),
+    isOfficerAssignments(obj.officers),
+    isWatchCrews(obj.watchCrews),
+    isProvisions(obj.provisions),
+    isNumber(obj.stock)
+  ].every(test => test)
+}
+
+export const createCrewData = (
+  overrides?: Partial<CrewData>
+): CrewData => {
+  const t = scopeLocalizer([MODULE_ID, 'watch-crews'].join('.'))
+  return {
+    articles: [],
+    officers: createOfficerAssignments(overrides?.officers),
+    watchCrews: [
+      { name: t('starboard'), lead: 'quartermaster', members: [], onDuty: true },
+      { name: t('larboard'), lead: 'master', members: [], onDuty: true }
+    ],
+    provisions: createProvisions(overrides?.provisions),
+    stock: 0,
+    ...overrides
+  }
+}
