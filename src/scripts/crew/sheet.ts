c22522ce537092b241b8585e3da99339caa2f6dc
Jason Godesky <jefgodesky@gmail.com>
2026-08-04 17:12:49 -0400

    PirateCrewSheet: Add tabs

--- src/scripts/crew/sheet.ts
+++ src/scripts/crew/sheet.ts
@@ -3,11 +3,11 @@ import { MODULE_ID } from '../settings.ts'
 import CrewModel from './schema.ts'
 
 class PirateCrewSheet extends foundry.applications.api.HandlebarsApplicationMixin(foundry.applications.sheets.ActorSheetV2) {
-  editing: boolean = this.isEditable
+  editing: boolean = false
 
   static DEFAULT_OPTIONS = {
     classes: ['pirate-crew'],
-    position: generateApplicationPosition({ height: 2/3, width: 1/3 }),
+    position: generateApplicationPosition({ height: 2/3, width: 750 }),
     window: { resizable: true },
     actions: {
       toggleEdit: PirateCrewSheet.toggleEdit
@@ -17,10 +17,33 @@ class PirateCrewSheet extends foundry.applications.api.HandlebarsApplicationMixi
     }
   }
 
+  static TABS = {
+    primary: {
+      tabs: [
+        { id: 'articles', icon: 'fa-solid fa-feather-pointed' },
+        { id: 'officers', icon: 'fa-solid fa-skull-crossbones' },
+        { id: 'watchCrews', icon: 'fa-solid fa-anchor' },
+        { id: 'provisions', icon: 'fa-solid fa-cookie' },
+        { id: 'assets', icon: 'fa-solid fa-sailboat' }
+      ],
+      initial: 'articles',
+      labelPrefix: `${MODULE_ID}.sheet.tabs`
+    }
+  }
+
   static PARTS = {
     header: { template: `modules/${MODULE_ID}/templates/sheets/crew/header.hbs` },
-    view: { template: `modules/${MODULE_ID}/templates/sheets/crew/view.hbs` },
-    edit: { template: `modules/${MODULE_ID}/templates/sheets/crew/edit.hbs` }
+    tabs: { template: 'templates/generic/tab-navigation.hbs' },
+    articlesView: { template: `modules/${MODULE_ID}/templates/sheets/crew/articles/view.hbs` },
+    articlesEdit: { template: `modules/${MODULE_ID}/templates/sheets/crew/articles/edit.hbs` },
+    officersView: { template: `modules/${MODULE_ID}/templates/sheets/crew/officers/view.hbs` },
+    officersEdit: { template: `modules/${MODULE_ID}/templates/sheets/crew/officers/edit.hbs` },
+    watchCrewsView: { template: `modules/${MODULE_ID}/templates/sheets/crew/watchCrews/view.hbs` },
+    watchCrewsEdit: { template: `modules/${MODULE_ID}/templates/sheets/crew/watchCrews/edit.hbs` },
+    provisionsView: { template: `modules/${MODULE_ID}/templates/sheets/crew/provisions/view.hbs` },
+    provisionsEdit: { template: `modules/${MODULE_ID}/templates/sheets/crew/provisions/edit.hbs` },
+    assetsView: { template: `modules/${MODULE_ID}/templates/sheets/crew/assets/view.hbs` },
+    assetsEdit: { template: `modules/${MODULE_ID}/templates/sheets/crew/assets/edit.hbs` },
   }
 
   get isEditing () {
@@ -31,7 +54,13 @@ class PirateCrewSheet extends foundry.applications.api.HandlebarsApplicationMixi
     super._configureRenderOptions(options)
     options.parts = ['header']
     if (this.document.limited) return
-    options.parts.push(this.isEditing ? 'edit' : 'view')
+
+    options.parts.push('tabs')
+
+    const mode = this.isEditing ? 'Edit' : 'View'
+    for (const tab of PirateCrewSheet.TABS.primary.tabs) {
+      options.parts.push(tab.id + mode)
+    }
   }
 
   async _prepareContext (options: Record<string, any>) {
@@ -42,7 +71,13 @@ class PirateCrewSheet extends foundry.applications.api.HandlebarsApplicationMixi
     context.fields = system.schema.fields
     context.editable = this.isEditable
     context.editing = this.isEditing
+    context.tabs = this._prepareTabs('primary')
     return context
+  }
+
+  _onClose (options: Record<string, any>) {
+    super._onClose(options)
+    this.editing = false
   }
 
   static async toggleEdit (this: PirateCrewSheet) {
