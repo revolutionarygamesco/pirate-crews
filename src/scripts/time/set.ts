9877cb6b326a8ba98f6688ddce536cbd43670e79
Jason Godesky <jefgodesky@gmail.com>
2026-08-14 21:20:18 -0400

    Add new class: WatchSet

---
+++ src/scripts/time/set.ts
@@ -0,0 +1,71 @@
+import { scopeLocalizer } from '@revolutionarygamesco/common-foundryvtt'
+import { MODULE_ID } from '../settings.ts'
+import { type Watch } from './types/watch.ts'
+import { type WatchSetData, SECONDS_PER_HOUR } from './types/set.ts'
+import { type WatchInstance } from './types/instance.ts'
+import isInWatchInstance from './methods/in-instance.ts'
+
+export default class WatchSet {
+  watches: Watch[]
+  offset: number
+
+  constructor (options?: Partial<WatchSetData>) {
+    const t = scopeLocalizer([MODULE_ID, 'watches', 'default'].join('.'))
+    const standards = ['first', 'middle', 'morning', 'forenoon', 'afternoon']
+      .map(node => ({ name: t(node), duration: 4 * SECONDS_PER_HOUR }))
+    const dogs = ['dog1', 'dog2']
+      .map(node => ({ name: t(node), duration: 2 * SECONDS_PER_HOUR }))
+
+    this.offset = options?.offset ?? -4 * SECONDS_PER_HOUR
+    this.watches = options?.watches ?? [...standards, ...dogs]
+  }
+
+  get dayLength () {
+    return this.watches.reduce((acc, curr) => acc + curr.duration, 0)
+  }
+
+  getInstances (timestamp: number): WatchInstance[] {
+    const day = Math.floor((timestamp - this.offset) / this.dayLength)
+    let start = (day * this.dayLength) + this.offset
+    const instances: WatchInstance[] = []
+
+    for (const watch of this.watches) {
+      instances.push({ ...watch, start, end: start + watch.duration })
+      start += watch.duration
+    }
+
+    return instances
+  }
+
+  getCurrent (timestamp: number): WatchInstance {
+    const instances = this.getInstances(timestamp)
+    for (const instance of instances) {
+      if (isInWatchInstance(instance, timestamp)) return instance
+    }
+    return instances[instances.length - 1]
+  }
+
+  getInstancesBetween (start: number, end: number): WatchInstance[] {
+    const first = this.getCurrent(start)
+    const instances: WatchInstance[] = [first]
+    let timestamp = first.end
+    let index = this.watches.findIndex(watch => watch.name === first.name)
+    if (index < 0) return instances
+
+    while (timestamp < end) {
+      index++
+      const watch = this.watches[index % this.watches.length]
+      instances.push({ ...watch, start: timestamp, end: timestamp + watch.duration })
+      timestamp += watch.duration
+    }
+
+    return instances
+  }
+
+  toObject (): WatchSetData {
+    return {
+      watches: this.watches,
+      offset: this.offset
+    }
+  }
+}
