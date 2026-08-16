1254a6e6d41da418628b032d77a4425b5f9009b6
Jason Godesky <jefgodesky@gmail.com>
2026-08-16 10:56:29 -0400

    Add new class: Crew

---
+++ src/scripts/crew/crew.test.ts
@@ -0,0 +1,205 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { mockActor } from '@revolutionarygamesco/common-foundryvtt/mocks'
+import setupRanger from '../ranger.ts'
+import { createSpecializationDefinitions } from './types/definitions.ts'
+import WatchSet from '../time/set.ts'
+import Crew from './crew.ts'
+
+const worldTimeMock = vi.fn()
+game.time = { get worldTime () { return worldTimeMock() } } as unknown as foundry.helpers.GameTime
+vi.mock('../time/foundry/load.ts', () => ({
+  default: () => new WatchSet()
+}))
+vi.mock('./foundry/load.ts', () => ({
+  default: () => createSpecializationDefinitions()
+}))
+
+describe('Crew', () => {
+  describe('constructor', () => {
+    it('creates a crew', () => {
+      expect(new Crew()).toBeInstanceOf(Crew)
+    })
+
+    it('loads the ship (as an actor)', () => {
+      const { crew: data } = setupRanger('Actor')
+      const crew = new Crew(data)
+      expect(crew.ship).toBeDefined()
+    })
+
+    it('loads the ship (as an item)', () => {
+      const { crew: data } = setupRanger('Item')
+      const crew = new Crew(data)
+      expect(crew.ship).toBeDefined()
+    })
+
+    it('loads the articles', () => {
+      const { crew: data } = setupRanger('Item')
+      const crew = new Crew(data)
+      expect(crew.articles).toBeDefined()
+    })
+
+    it('loads specialists', () => {
+      const { crew: data } = setupRanger('Item')
+      const crew = new Crew(data)
+      expect(crew.specialists.has('captain')).toBe(true)
+      expect(crew.specialists.has('quartermaster')).toBe(true)
+    })
+
+    it('loads watch crews', () => {
+      const { crew: data } = setupRanger('Item')
+      const crew = new Crew(data)
+      expect(crew.starboard).toHaveLength(1)
+      expect(crew.larboard).toHaveLength(1)
+    })
+  })
+
+  describe('Accessor methods', () => {
+    let crew: Crew
+
+    beforeEach(() => {
+      const { crew: data } = setupRanger()
+      crew = new Crew(data)
+    })
+
+    describe('idlers', () => {
+      it('returns the idler actors', () => {
+        expect(crew.idlers.map(actor => actor.name)).toEqual([
+          'Charles Vane',
+          '“Calico” Jack Rackham'
+        ])
+      })
+    })
+
+    describe('onDuty', () => {
+      it('returns the actors who are on duty', () => {
+        worldTimeMock.mockReturnValue(9 * 60 * 60)
+        expect(crew.onDuty.map(actor => actor.name)).toEqual([
+          'Anne Bonny',
+          'Charles Vane',
+          '“Calico” Jack Rackham'
+        ])
+      })
+    })
+
+    describe('offDuty', () => {
+      it('returns the actors who are off duty', () => {
+        worldTimeMock.mockReturnValue(100)
+        expect(crew.offDuty.map(actor => actor.name)).toEqual([
+          'Mary Read',
+          'Charles Vane',
+          '“Calico” Jack Rackham'
+        ])
+      })
+    })
+
+    describe('all', () => {
+      it('returns all crew members', () => {
+        expect(crew.all.map(actor => actor.name)).toEqual([
+          'Charles Vane',
+          '“Calico” Jack Rackham',
+          'Anne Bonny',
+          'Mary Read'
+        ])
+      })
+    })
+
+    describe('count', () => {
+      it('returns the number of people on the crew', () => {
+        expect(crew.count).toBe(4)
+      })
+    })
+  })
+
+  describe('Instance methods', () => {
+    let crew: Crew
+
+    beforeEach(() => {
+      const { crew: data } = setupRanger()
+      crew = new Crew(data)
+    })
+
+    describe('getRandomMember', () => {
+      it('returns a random member of the crew', () => {
+        const expected = ['Charles Vane', '“Calico” Jack Rackham', 'Anne Bonny', 'Mary Read']
+        const actual = crew.getRandomMember()
+        expect(expected).toContain(actual.name)
+      })
+    })
+
+    describe('getRandomOfficer', () => {
+      it('returns the captain, quartermaster, sailing master, or bosun', () => {
+        const expected = ['Charles Vane', '“Calico” Jack Rackham']
+        const actual = crew.getRandomOfficer()
+        expect(expected).toContain(actual.name)
+      })
+    })
+
+    describe('getRandomOnDuty', () => {
+      it('returns a randomly-selected on-duty member of the crew', () => {
+        worldTimeMock.mockReturnValue(9 * 60 * 60)
+        const expected = ['Anne Bonny', 'Charles Vane', '“Calico” Jack Rackham']
+        const actual = crew.getRandomOnDuty()
+        expect(expected).toContain(actual.name)
+      })
+    })
+
+    describe('getRandomOffDuty', () => {
+      it('returns a randomly-selected off-duty member of the crew', () => {
+        worldTimeMock.mockReturnValue(100)
+        const expected = ['Mary Read', 'Charles Vane', '“Calico” Jack Rackham']
+        const actual = crew.getRandomOffDuty()
+        expect(expected).toContain(actual.name)
+      })
+    })
+
+    describe('add', () => {
+      let actor: foundry.documents.Actor
+
+      beforeEach(() => {
+        actor = mockActor({ name: 'John Doe' })
+      })
+
+      it('can add an actor to the starboard crew', () => {
+        crew.add(actor, 'starboard')
+        expect(crew.starboard.map(({ uuid }) => uuid)).toContain(actor.uuid)
+      })
+
+      it('can add an actor to the larboard crew', () => {
+        crew.add(actor, 'larboard')
+        expect(crew.larboard.map(({ uuid }) => uuid)).toContain(actor.uuid)
+      })
+
+      it('can add an actor as a specialist', () => {
+        crew.add(actor, 'bosun')
+        expect(crew.specialists.get('bosun')?.actor).toBe(actor.uuid)
+      })
+    })
+
+    describe('remove', () => {
+      it('can remove a member of the starboard watch crew', () => {
+        crew.remove(crew.starboard[0].uuid ?? '')
+        expect(crew.starboard).toHaveLength(0)
+      })
+
+      it('can remove a member of the larboard watch crew', () => {
+        crew.remove(crew.larboard[0].uuid ?? '')
+        expect(crew.larboard).toHaveLength(0)
+      })
+
+      it('can remove an officer', () => {
+        crew.remove(crew.specialists.get('quartermaster')?.actor ?? '')
+        expect(crew.specialists.has('quartermaster')).toBe(false)
+        expect(crew.starboard).toHaveLength(1)
+        expect(crew.larboard).toHaveLength(1)
+      })
+    })
+
+    describe('decommission', () => {
+      it('moves a specialist onto a watch team', () => {
+        crew.decommission('quartermaster')
+        expect(crew.specialists.has('quartermaster')).toBe(false)
+        expect(crew.starboard).toHaveLength(2)
+      })
+    })
+  })
+})
---
+++ src/scripts/crew/crew.ts
@@ -0,0 +1,173 @@
+import { selectRandomElement, isString } from '@revolutionarygamesco/common'
+import { getID } from '@revolutionarygamesco/common-foundryvtt'
+import { type CrewData } from './types/data.ts'
+import { type Specialization } from './types/specialization.ts'
+import { type WatchInstance } from '../time/types/instance.ts'
+import { isWatchTeam, type WatchTeam } from './types/team.ts'
+import getOtherTeam from './methods/other-team.ts'
+import loadSpecializationDefinitions from './foundry/load.ts'
+import loadWatches from '../time/foundry/load.ts'
+
+class Crew {
+  ship?: foundry.documents.Actor | foundry.documents.Item
+  articles?: foundry.documents.JournalEntry
+  specialists: Map<string, Specialization>
+  starboard: foundry.documents.Actor[]
+  larboard: foundry.documents.Actor[]
+
+  constructor (data?: Partial<CrewData>) {
+    this.ship = data?.ship
+      ? data.ship.startsWith('Actor')
+        ? game.actors.get(getID(data.ship))
+        : game.items.get(getID(data.ship))
+      : undefined
+    this.articles = data?.articles
+      ? game.journal.get(getID(data.articles))
+      : undefined
+    this.specialists = new Map<string, Specialization>()
+    this.starboard = []
+    this.larboard = []
+
+    if (data?.specialists) {
+      for (const key in data.specialists) {
+        this.specialists.set(key, data.specialists[key])
+      }
+    }
+
+    const teams: Array<{ src: string[], dest: foundry.documents.Actor[] }> = [
+      { src: data?.teams?.starboard ?? [], dest: this.starboard },
+      { src: data?.teams?.larboard ?? [], dest: this.larboard }
+    ]
+
+    for (const { src, dest } of teams) {
+      for (const uuid of src) {
+        const actor = game.actors.get(getID(uuid))
+        if (actor) dest.push(actor)
+      }
+    }
+  }
+
+  get idlers (): foundry.documents.Actor[] {
+    return this.mapSpecializationsToActors()
+  }
+
+  get onDuty (): foundry.documents.Actor[] {
+    const { team, idlers } = this.getWatch()
+    return idlers
+      ? [...this[team], ...this.idlers]
+      : this[team]
+  }
+
+  get offDuty (): foundry.documents.Actor[] {
+    const { team, idlers } = this.getWatch()
+    const off = getOtherTeam(team)
+    return !idlers
+      ? [...this[off], ...this.idlers]
+      : this[off]
+  }
+
+  get all (): foundry.documents.Actor[] {
+    const specialists = this.mapSpecializationsToActors()
+    const iterable = [...specialists, ...this.starboard, ...this.larboard]
+      .map(actor => [actor.uuid, actor] as [string, foundry.documents.Actor])
+    const actors = new Map<string, foundry.documents.Actor>(iterable)
+    return Array.from(actors.values())
+  }
+
+  get count (): number {
+    return this.all.length
+  }
+
+  getRandomMember (): foundry.documents.Actor {
+    return selectRandomElement(this.all)
+  }
+
+  getRandomOfficer (): foundry.documents.Actor {
+    const candidates = this.mapSpecializationsToActors(['captain', 'quartermaster', 'master', 'bosun'])
+    return selectRandomElement(candidates)
+  }
+
+  getRandomOnDuty (): foundry.documents.Actor {
+    return selectRandomElement(this.onDuty)
+  }
+
+  getRandomOffDuty (): foundry.documents.Actor {
+    return selectRandomElement(this.offDuty)
+  }
+
+  add (
+    actor: foundry.documents.Actor | string,
+    position: WatchTeam | string
+  ): void {
+    const a = isString(actor) ? game.actors.get(getID(actor)): actor
+    if (!a || !a.uuid) return
+
+    if (isWatchTeam(position)) {
+      this[position].push(a)
+    } else {
+      if (this.specialists.has(position)) {
+        const spec = this.specialists.get(position)!
+        if (spec.actor) this.decommission(position)
+        this.specialists.set(position, { ...spec, actor: a.uuid })
+      } else {
+        const defs = loadSpecializationDefinitions()
+        if (!defs[position]) return
+        this.specialists.set(position, {
+          ...defs[position],
+          actor: a.uuid
+        })
+      }
+    }
+  }
+
+  remove (
+    actor: foundry.documents.Actor | string
+  ): void {
+    const a = isString(actor) ? game.actors.get(getID(actor)): actor
+    if (!a || !a.uuid) return
+
+    let specialization = ''
+    for (const [key, value] of this.specialists.entries()) {
+      if (value.actor === a.uuid) specialization = key
+    }
+
+    if (specialization !== '') this.specialists.delete(specialization)
+
+    this.starboard = this.starboard.filter(actor => actor.uuid !== a.uuid)
+    this.larboard = this.larboard.filter(actor => actor.uuid !== a.uuid)
+  }
+
+  decommission (
+    key: string
+  ): void {
+    const prev = this.specialists.get(key)
+    if (!prev) return
+
+    const actor = game.actors.get(getID(prev.actor ?? ''))
+    if (!actor) return
+
+    this.specialists.delete(key)
+    const bigger = this.larboard.length < this.starboard.length
+      ? 'larboard' : 'starboard'
+    this[bigger].push(actor)
+  }
+
+  protected getWatch (): WatchInstance {
+    const set = loadWatches()
+    return set.getCurrent(game.time.worldTime)
+  }
+
+  protected mapSpecializationsToActors (keys?: string[]): foundry.documents.Actor[] {
+    const specializations = keys && keys.length > 0
+      ? keys.map(key => this.specialists.get(key))
+      : Array.from(this.specialists.values())
+    return specializations
+      .filter(spec => spec !== undefined)
+      .map(({ actor }) => actor)
+      .filter(uuid => uuid !== undefined)
+      .map(uuid => game.actors.get(getID(uuid)))
+      .filter(actor => actor !== undefined)
+  }
+}
+
+export default Crew
