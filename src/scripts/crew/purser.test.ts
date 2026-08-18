0c5b852bedd4a11047705d161634b0d82dacb0cb
Jason Godesky <jefgodesky@gmail.com>
2026-08-17 21:06:48 -0400

    Purser.estimate

--- src/scripts/crew/purser.test.ts
+++ src/scripts/crew/purser.test.ts
@@ -22,4 +22,35 @@ describe('Purser', () => {
       expect(actual.crew).toBe(crew)
     })
   })
+
+  describe('Instance methods', () => {
+    describe('estimate', () => {
+      let purser: Purser
+
+      beforeEach(() => {
+        crew.stock = 500
+        purser = new Purser(crew)
+      })
+
+      it('calculates the amount to pay out to each member of the crew', () => {
+        expect(purser.estimate(345)).toEqual({
+          [crew.specialists.get('captain')!.actor!]: 114, // Captain Charles Vane
+          [crew.specialists.get('quartermaster')!.actor!]: 114, // Quartermaster “Calico” Jack Rackham
+          [crew.starboard[0].uuid!]: 57, // Crew member Anne Bonny
+          [crew.larboard[0].uuid!]: 57, // Crew member Mary Read
+          total: 114 + 114 + 57 + 57
+        })
+      })
+
+      it('won’t let you pay out more than you have in common stock', () => {
+        expect(purser.estimate(5000000)).toEqual({
+          [crew.specialists.get('captain')!.actor!]: 166, // Captain Charles Vane
+          [crew.specialists.get('quartermaster')!.actor!]: 166, // Quartermaster “Calico” Jack Rackham
+          [crew.starboard[0].uuid!]: 83, // Crew member Anne Bonny
+          [crew.larboard[0].uuid!]: 83, // Crew member Mary Read
+          total: 166 + 166 + 83 + 83
+        })
+      })
+    })
+  })
 })
--- src/scripts/crew/purser.ts
+++ src/scripts/crew/purser.ts
@@ -6,6 +6,21 @@ class Purser {
   constructor (crew: Crew) {
     this.crew = crew
   }
+
+  estimate (target: number): Record<string, number> {
+    const shares = this.crew.shares
+    const adjustedTarget = Math.min(target, this.crew.stock)
+    const perShare = Math.floor(adjustedTarget / shares.total)
+    const ledger: Record<string, number> = { total: 0 }
+
+    for (const uuid of this.crew.uuids) {
+      const amount = Math.floor(shares[uuid] * perShare)
+      ledger[uuid] = amount
+      ledger.total += amount
+    }
+
+    return ledger
+  }
 }
 
 export default Purser
